67caaf09d1f9feef5932188805b5b2072dd71564
Ayush-Gurav <ayushgurav6@gmail.com>
2024-04-21 01:13:45 +0530

    Categories_basic

---
+++ src/components/Navbar.js
@@ -0,0 +1,12 @@
+import { AppBar, Toolbar } from "@mui/material";
+
+function Navbar() {
+  return (
+    <AppBar position="static">
+      <Toolbar>
+        <p>Small BiZZ</p>
+      </Toolbar>
+    </AppBar>
+  );
+}
+export default Navbar;
